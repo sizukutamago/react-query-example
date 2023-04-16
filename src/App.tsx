1e7706c110ede4b74aa3bb48f13bb49d7adecbbe
sizukutamago <sizukutamago@gmail.com>
2023-04-16 13:39:43 +0900

    useQuery

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useMutation, useQuery, useQueryClient } from "react-query";
 
 function App() {
+  const getTodos = async (): Promise<
+    {
+      userId: string;
+      id: string;
+      title: string;
+      completed: boolean;
+    }[]
+  > => {
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/todos"
+      );
+
+      return await response.json();
+    } catch (error) {
+      console.error(error);
+      return [];
+    }
+  };
+
+  const postTodo = async ({ id, title }: { id: string; title: string }) => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      body: JSON.stringify({ title, userId: id }),
+    });
+
+    return await response.json();
+  };
+
+  const queryClient = useQueryClient();
+
+  const query = useQuery({ queryKey: ["todos"], queryFn: getTodos });
+
+  // Mutations
+  const mutation = useMutation({
+    mutationFn: postTodo,
+    onSuccess: () => {
+      // Invalidate and refetch
+      queryClient.invalidateQueries({ queryKey: ["todos"] });
+    },
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        {query.data &&
+          query.data.map((todo) => <li key={todo.id}>{todo.title}</li>)}
+      </ul>
+
+      <button
+        onClick={() => {
+          mutation.mutate({
+            id: Date.now().toString(),
+            title: "Do Laundry",
+          });
+        }}
+      >
+        Add Todo
+      </button>
     </div>
   );
 }
